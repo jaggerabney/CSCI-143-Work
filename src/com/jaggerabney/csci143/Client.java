9d1a607212d0b4664cda2fc87ceed706351f9468
Jagger Abney <jagger.abney@gmail.com>
2021-09-27 22:53:26 -0700

    implemented solution to chap 9 project 1

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -4,9 +4,12 @@ import com.jaggerabney.csci143.week1.*;
 
 public class Client {
     public static void main(String[] args) {
-        MyInteger mi1 = new MyInteger(2);
-        MyInteger mi2 = new MyInteger(3);
-        System.out.println(mi1.isEven());
-        System.out.println(mi2.isEven());
+        MyRectangle r1 = new MyRectangle(4, 40);
+        MyRectangle r2 = new MyRectangle(3.5, 35.9);
+        
+        System.out.println("The area of a rectangle with width " + r1.width + " and height " + r1.height + " is " + r1.getArea());
+        System.out.println("The perimeter of a rectangle is " + r1.getPerimeter());
+        System.out.println("The area of a rectangle with width " + r2.width + " and height " + r2.height + " is " + r2.getArea());
+        System.out.println("The perimeter of a rectangle is " + r2.getPerimeter());
     }
 }
