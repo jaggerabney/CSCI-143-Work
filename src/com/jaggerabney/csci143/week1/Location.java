1535c39eada63f0278e97faf28cfd2cb8429a693
Jagger Abney <jagger.abney@gmail.com>
2021-09-29 18:53:16 -0700

    implemented basic Location class

---
+++ src/com/jaggerabney/csci143/week1/Location.java
@@ -0,0 +1,12 @@
+package com.jaggerabney.csci143.week1;
+
+public class Location {
+  public int row, column;
+  public double maxValue;
+
+  public Location(int row, int column, double maxValue) {
+    this.row = row;
+    this.column = column;
+    this.maxValue = maxValue;
+  }
+}
