7d57014f1e3e138f74197f429fd17852ea9b775b
Jagger Abney <jagger.abney@gmail.com>
2021-10-04 20:59:28 -0700

    added BankAccount/CheckingAccount

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -4,7 +4,7 @@ import com.jaggerabney.csci143.week1.assignment.*;
 
 public class Client {
   public static void main(String[] args) {
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 11; i++) {
       MyInteger number = new MyInteger(i);
 
       System.out.println(number.get());
