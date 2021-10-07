7ea496d1cd0acf4302c04024710b58aca14bc9ce
Jagger Abney <jagger.abney@gmail.com>
2021-10-07 12:22:47 -0700

    partially implemented PerfectSquare class

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -6,18 +6,9 @@ import java.util.*;
 public class Client {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int numPoints;
-    double[] points;
+    int m, n;
 
-    System.out.println("Enter the number of points: ");
-    numPoints = input.nextInt();
-    points = new double[numPoints * 2];
-    System.out.println("Enter the coordinates of the points: ");
-    for (int i = 0; i < points.length; i++) {
-      points[i] = input.nextDouble();
-    }
-
-    ConvexPolygon cp = new ConvexPolygon(points);
-    System.out.println("The total area is " + cp.getArea());
+    System.out.println("Enter an integer m: ");
+    m = input.nextInt();
   }
 }
---
+++ src/com/jaggerabney/csci143/week2/projects/PerfectSquare.java
@@ -0,0 +1,45 @@
+package com.jaggerabney.csci143.week2.projects;
+
+import java.util.ArrayList;
+
+public class PerfectSquare {
+  public static int[] primeFactorizationsOf(int value) {
+    int divisor = 2;
+    ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+
+    while (value > 0) {
+      if ((double) value % (double) divisor == 0.0) {
+        primeFactors.add(divisor);
+        value /= divisor;
+      } else {
+        divisor = nextPrimeAfter(divisor);
+      }
+    }
+
+    return primeFactors.stream().mapToInt(i -> i).toArray();
+  }
+
+  public static int nextPrimeAfter(int value) {
+    while (!isPrime(value)) {
+      value++;
+    }
+    return value;
+  }
+
+  public static boolean isPrime(int value) {
+    boolean isPrime = true;
+
+    if (value >= 2) {
+      for (int i = 2; i < value; i++) {
+        if (value % i == 0) { // if value is divisible by anything
+          isPrime = false;
+        }
+      }
+    } else {
+      isPrime = false; // apparently 0, 1, and negative numbers are not prime
+      // this else statement reflects that admittedly weird mathematic behavior
+    }
+
+    return isPrime;
+  }
+}
