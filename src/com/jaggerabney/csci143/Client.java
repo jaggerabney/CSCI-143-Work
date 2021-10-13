baaa7c603310bba886b3161aebfbcaa5eff02ca7
Jagger Abney <jagger.abney@gmail.com>
2021-10-13 16:52:19 -0700

    condensed code in Client class

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -4,16 +4,11 @@ import com.jaggerabney.csci143.week2.classcoding.*;
 
 public class Client {
   public static void main(String[] args) {
-    Employee employee = new Employee();
-    Marketer marketer = new Marketer();
-    Lawyer lawyer = new Lawyer();
-    Secretary secretary = new Secretary();
-    LegalSecretary legalSecretary = new LegalSecretary();
+    Employee[] employees = new Employee[] { new Employee(), new Lawyer(), new Marketer(), new Secretary(),
+        new LegalSecretary() };
 
-    System.out.println(employee);
-    System.out.println(marketer);
-    System.out.println(lawyer);
-    System.out.println(secretary);
-    System.out.println(legalSecretary);
+    for (Employee employee : employees) {
+      System.out.println(employee);
+    }
   }
 }
