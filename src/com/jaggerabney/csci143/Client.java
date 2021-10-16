ce806a23a147170f05a22edd7febb438de3a90ce
Jagger Abney <jagger.abney@gmail.com>
2021-10-16 11:31:23 -0700

    disabled main method in Client class

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -1,14 +1,4 @@
 package com.jaggerabney.csci143;
 
-import com.jaggerabney.csci143.week2.classcoding.*;
-
 public class Client {
-  public static void main(String[] args) {
-    Employee[] employees = new Employee[] { new Employee(5), new Lawyer(2), new Marketer(10), new Secretary(4),
-        new LegalSecretary(9) };
-
-    for (Employee employee : employees) {
-      System.out.println(employee);
-    }
-  }
 }
