5ac4ad27f11585e51e2c29f4524b5480f906a9cd
Jagger Abney <jagger.abney@gmail.com>
2021-10-16 12:57:07 -0700

    commented Mouse and Turtle

--- src/com/jaggerabney/csci143/week2/groupproject/Mouse.java
+++ src/com/jaggerabney/csci143/week2/groupproject/Mouse.java
@@ -1,9 +1,15 @@
 package com.jaggerabney.csci143.week2.groupproject;
 
+// File: Mouse.java
+// Author: Jagger Abney
+// Purpose: to define a Critter that constantly moves northwest
+
 public class Mouse implements Critter {
+  // keeps track of the last direction the Mouse moved in
   private int lastMoveDirection;
 
   public Mouse() {
+    // first move is to the West
     this.lastMoveDirection = Critter.WEST;
   }
 
@@ -14,6 +20,8 @@ public class Mouse implements Critter {
 
   @Override
   public int getMove(CritterInfo info) {
+    // moves North if last direction was West, and vice versa
+    // this results in a northwest zig-zag pattern
     if (lastMoveDirection == Critter.WEST) {
       lastMoveDirection = Critter.NORTH;
       return Critter.NORTH;
--- src/com/jaggerabney/csci143/week2/groupproject/Turtle.java
+++ src/com/jaggerabney/csci143/week2/groupproject/Turtle.java
@@ -1,5 +1,11 @@
 package com.jaggerabney.csci143.week2.groupproject;
 
+// File: Turtle.java
+// Author: Jagger Abney
+// Purpose: to define a Critter that moves in a clockwise, 5-step box
+
+import java.util.Random;
+
 public class Turtle implements Critter {
   private Movement movementInfo;
 
@@ -14,29 +20,45 @@ public class Turtle implements Critter {
 
   @Override
   public int getMove(CritterInfo info) {
+    // which direction the Turtle moves in is handled by its Movement class
     return movementInfo.step();
   }
 
-  class Movement {
+  private class Movement {
+    // fields for the amount of steps to take before turning,
+    // how many steps have been taken before the next turn,
+    // and the last direction turned to
     public int defaultSteps, stepsToTurn, lastMoveDirection;
 
     public Movement(int defaultSteps, int lastMoveDirection) {
       this.defaultSteps = defaultSteps;
       this.stepsToTurn = defaultSteps;
-      this.lastMoveDirection = lastMoveDirection;
+      this.lastMoveDirection = randomDirection();
     }
 
     public int step() {
       if (stepsToTurn <= 0) {
+        // if there are no more steps to take before the turn, reset the
+        // Turtle's steps to the default (i.e. 5) and turn
         stepsToTurn = defaultSteps;
         lastMoveDirection = turn();
       }
 
+      // finally, subtract steps taken and return the direction to move in
       stepsToTurn--;
       return lastMoveDirection;
     }
 
+    public int randomDirection() {
+      // generates a random direction -- used in the Turtle's initialization
+      int[] possibilities = new int[] { Critter.NORTH, Critter.EAST, Critter.SOUTH, Critter.WEST };
+      Random rng = new Random();
+      return possibilities[rng.nextInt(possibilities.length - 1)];
+    }
+
     public int turn() {
+      // returns a direction based on which direction the Turtle moved in last
+      // the Turtle moves clockwise
       switch (lastMoveDirection) {
         case Critter.SOUTH:
           return Critter.WEST;
