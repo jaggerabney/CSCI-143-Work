94cad70b2bd28c62c98df81e2ebc2f39ac437156
Jagger Abney <jagger.abney@gmail.com>
2021-10-22 22:11:25 -0700

    finished implementation and added comments

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -1,13 +1,4 @@
 package com.jaggerabney.csci143;
 
-import java.util.*;
-
-import com.jaggerabney.csci143.week4.assignment.*;
-
 public class Client {
-  public static void main(String[] args) {
-    ArrayList<String> list = new ArrayList<>(
-        Arrays.asList("mankind", "knew", "that", "they", "couldn't", "change", "society"));
-    System.out.println(ReverseFile.reverse(list));
-  }
 }
--- src/com/jaggerabney/csci143/week4/assignment/ReverseFile.java
+++ src/com/jaggerabney/csci143/week4/assignment/ReverseFile.java
@@ -1,3 +1,9 @@
+/**
+ * Filename: ReverseFile.java
+ * Author: Jagger Abney
+ * Purpose: to reverse the contents of a file 
+ */
+
 package com.jaggerabney.csci143.week4.assignment;
 
 import java.io.*;
@@ -6,23 +12,52 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReverseFile {
+  public static final String FILE_NAME = "mobydick.txt";
+
+  public static void main(String[] args) {
+    // try/catch block is used because BufferedReader's readLine method can throw an
+    // IOException
+    try {
+      // reads the contents of the file into an ArrayList
+      ArrayList<String> fileContents = readFile(FILE_NAME);
+      // reverses the fileContents ArrayList
+      ArrayList<String> reversedFileContents = reverse(fileContents);
+      // writes the reversed fileContents ArrayList to the file
+      writeToFile(reversedFileContents);
+    } catch (Exception e) {
+      // prints the stack trace if IOException is caught
+      // if this happens, make sure that mobydick.txt is in the same directory as
+      // this file!
+      e.printStackTrace();
+    }
+  }
+
   public static ArrayList<String> readFile(String filename) throws Exception {
+    // creates an InputStream, InputStreamReader, and BufferedReader (the former
+    // two are used to create the last, which is what is used to read the file)
     InputStream is = ReverseFile.class.getResourceAsStream(filename);
     InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
     BufferedReader br = new BufferedReader(isr);
+    // lines from the file are added to fileContents, and stored temporarily
+    // in line
     ArrayList<String> fileContents = new ArrayList<String>();
     String line;
 
+    // stores lines of the file into fileContents so long as there are lines
+    // to store
     while ((line = br.readLine()) != null) {
       fileContents.add(line);
     }
 
+    // returns the result
     return fileContents;
   }
 
   public static ArrayList<String> reverse(ArrayList<String> list) {
     ArrayList<String> reversedList = new ArrayList<String>();
 
+    // iterates through the passed list in reverse order, reversing the current line
+    // and adding it to the reversedList ArrayList
     for (int i = list.size() - 1; i >= 0; i--) {
       reversedList.add(reverse(list.get(i)));
     }
@@ -33,10 +68,26 @@ public class ReverseFile {
   public static String reverse(String string) {
     ArrayList<Character> reversedString = new ArrayList<>();
 
+    // iterates through the characters of the array and adds them to reversedString
     for (int i = string.length() - 1; i >= 0; i--) {
       reversedString.add(string.charAt(i));
     }
 
+    // converts the reversedString ArrayList to a string using a lambda & returns it
     return reversedString.stream().map(e -> e.toString()).collect(Collectors.joining());
+  }
+
+  public static void writeToFile(ArrayList<String> contents) throws Exception {
+    // creates a FileWriter for use in writing to the file
+    FileWriter fw = new FileWriter(FILE_NAME);
+
+    // writes each line of the contents ArrayList to the file, inserting line breaks
+    // between elements to preserve the formatting of the file
+    for (String line : contents) {
+      fw.write(line + "\n");
+    }
+
+    // closes the FileWriter
+    fw.close();
   }
 }
