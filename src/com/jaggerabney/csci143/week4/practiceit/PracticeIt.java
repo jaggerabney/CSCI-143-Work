4d772896e2cda116fa8d1cbd76693c82189ae40f
Jagger Abney <jagger.abney@gmail.com>
2021-10-22 19:27:04 -0700

    implemented markLength4 method

--- src/com/jaggerabney/csci143/week4/practiceit/PracticeIt.java
+++ src/com/jaggerabney/csci143/week4/practiceit/PracticeIt.java
@@ -102,4 +102,13 @@ public class PracticeIt {
       }
     }
   }
+
+  public static void markLength4(ArrayList<String> list) {
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).length() == 4) {
+        list.add(i, "****");
+        i++;
+      }
+    }
+  }
 }
