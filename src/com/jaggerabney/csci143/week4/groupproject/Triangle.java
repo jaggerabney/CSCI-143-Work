bcf1fbbb4280d5c7a9ba10e177198f3f7069ecac
Jagger Abney <jagger.abney@gmail.com>
2021-10-23 16:26:59 -0700

    changed default constructor in Triangle, commented

--- src/com/jaggerabney/csci143/week4/groupproject/Triangle.java
+++ src/com/jaggerabney/csci143/week4/groupproject/Triangle.java
@@ -7,9 +7,7 @@ public class Triangle extends GeometricObject {
 
     // No argument constructor which Initializes a deafault Triangle.
     public Triangle() {
-        this.side1 = 0.0;
-        this.side2 = 0.0;
-        this.side3 = 0.0;
+        this(0.0, 0.0, 0.0);
     }
 
     // Constructor which takes side values from user.
@@ -19,6 +17,7 @@ public class Triangle extends GeometricObject {
         this.side3 = s3;
     }
 
+    // getters for sides 1, 2, and 3
     public double getSide1() {
         return this.side1;
     }
@@ -33,12 +32,17 @@ public class Triangle extends GeometricObject {
 
     @Override
     public double getArea() {
+        // area is found by using Heron's formula:
+        // sqrt(s(s - a)(s - b)(s - c))
+        // where s is the semiperimeter of the triangle ((a + b + c) / 2),
+        // and a, b, and c are the three sides of the triangle
         double s = (side1 + side2 + side3) / 2.0;
         return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
 
     @Override
     public double getPerimeter() {
+        // perimeter is found by summing sides up
         return side1 + side2 + side3;
     }
 
