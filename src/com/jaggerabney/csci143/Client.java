c876e3bca6c9c7f247ba5a6a93c3ce2f8206d0ff
Jagger Abney <jagger.abney@gmail.com>
2021-10-27 21:13:27 -0700

    implemented starString

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -1,4 +1,12 @@
 package com.jaggerabney.csci143;
 
+import com.jaggerabney.csci143.week5.practiceit.*;
+
 public class Client {
+  public static void main(String[] args) {
+    int[] values = new int[] { 0, 1, 2, 3, 4 };
+    for (int i : values) {
+      System.out.println(PracticeIt.starString(i));
+    }
+  }
 }
--- src/com/jaggerabney/csci143/week5/practiceit/PracticeIt.java
+++ src/com/jaggerabney/csci143/week5/practiceit/PracticeIt.java
@@ -1,5 +1,13 @@
 package com.jaggerabney.csci143.week5.practiceit;
 
 public class PracticeIt {
-
+  public static String starString(int times) {
+    if (times == 0) {
+      return "*";
+    } else if (times > 0) {
+      return starString(times - 1) + starString(times - 1);
+    } else {
+      throw new IllegalArgumentException("times must be less than zero.");
+    }
+  }
 }
