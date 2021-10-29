b2df37e2d3479c90ae854ef1b00c7a21bce696bf
Jagger Abney <jagger.abney@gmail.com>
2021-10-28 21:29:42 -0700

    implemented BigIntFact

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -1,13 +1,12 @@
 package com.jaggerabney.csci143;
 
 import com.jaggerabney.csci143.week5.practiceit.*;
+import com.jaggerabney.csci143.week5.projects.*;
+import java.math.*;
 
 public class Client {
   public static void main(String[] args) {
-    int[] values = new int[] { 5, 1, 8 };
-    for (int i : values) {
-      PracticeIt.writeSquares(i);
-      System.out.println();
-    }
+    BigInteger bi = BigIntFact.factorial(25);
+    System.out.println(bi);
   }
 }
