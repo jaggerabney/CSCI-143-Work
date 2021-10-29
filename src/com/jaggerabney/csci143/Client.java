16e736f866d1a972fe1ad2f91e6ed6411da00bc5
Jagger Abney <jagger.abney@gmail.com>
2021-10-28 22:41:21 -0700

    implemented gcd in Exercise18_03

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -1,12 +1,19 @@
 package com.jaggerabney.csci143;
 
-import com.jaggerabney.csci143.week5.misc.*;
 import com.jaggerabney.csci143.week5.projects.*;
-import java.math.*;
+import java.util.*;
 
 public class Client {
   public static void main(String[] args) {
-    int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    System.out.println(Misc.sum(nums, 10));
+    int m, n;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter the first number: ");
+    m = scanner.nextInt();
+    System.out.print("Enter the second number: ");
+    n = scanner.nextInt();
+
+    System.out.println("The GCD of " + m + " and " + n + " is " + Exercise18_03.gcd(m, n));
+    scanner.close();
   }
 }
