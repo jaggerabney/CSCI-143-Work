f90dffa4b5a8f18baaac2350e9930d372ffda383
Jagger Abney <jagger.abney@gmail.com>
2021-10-31 12:16:21 -0700

    implemented TileList

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -3,9 +3,4 @@ package com.jaggerabney.csci143;
 import com.jaggerabney.csci143.week5.practiceit.*;
 
 public class Client {
-  public static void main(String[] args) {
-    String stringToSearch = "Barack Obama";
-    String stringToFind = "ck";
-    System.out.println(PracticeIt.indexOf(stringToSearch, stringToFind));
-  }
 }
--- src/com/jaggerabney/csci143/week5/groupproject/TileList.java
+++ src/com/jaggerabney/csci143/week5/groupproject/TileList.java
@@ -1,27 +1,46 @@
 package com.jaggerabney.csci143.week5.groupproject;
 
+import java.util.*;
+
 public class TileList {
+  private ArrayList<Tile> tiles;
+
   public TileList() {
+    this.tiles = new ArrayList<>();
   }
 
   public Tile moveToBack(int x, int y) {
-    return null;
+    Tile resultTile = null;
+
+    for (Tile tile : tiles) {
+      if (tile.inside(x, y)) {
+        resultTile = tile;
+      }
+    }
+
+    if (resultTile != null) {
+      tiles.remove(resultTile);
+      insertBack(resultTile);
+    }
+
+    return resultTile;
   }
 
   // post: inserts t at the back of the list of tiles
 
   public void insertBack(Tile t) {
+    tiles.add(t);
   }
 
   // post: returns the number of tiles in this list
 
   public int size() {
-    return 0;
+    return tiles.size();
   }
 
   // post: returns the Tile at the given index
 
   public Tile get(int index) {
-    return null;
+    return tiles.get(index);
   }
 }
