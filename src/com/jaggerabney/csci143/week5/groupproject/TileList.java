9e2c9b701a49330bd0287fb9d975cd64ac0b7115
Jagger Abney <jagger.abney@gmail.com>
2021-10-31 11:59:52 -0700

    added code for group project

---
+++ src/com/jaggerabney/csci143/week5/groupproject/TileList.java
@@ -0,0 +1,27 @@
+package com.jaggerabney.csci143.week5.groupproject;
+
+public class TileList {
+  public TileList() {
+  }
+
+  public Tile moveToBack(int x, int y) {
+    return null;
+  }
+
+  // post: inserts t at the back of the list of tiles
+
+  public void insertBack(Tile t) {
+  }
+
+  // post: returns the number of tiles in this list
+
+  public int size() {
+    return 0;
+  }
+
+  // post: returns the Tile at the given index
+
+  public Tile get(int index) {
+    return null;
+  }
+}
