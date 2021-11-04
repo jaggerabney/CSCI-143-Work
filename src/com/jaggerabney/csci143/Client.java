68a130a3a1fa37a154a4b2fbc3c59100c8f6e5a4
Jagger Abney <jagger.abney@gmail.com>
2021-11-04 00:34:25 -0700

    implemented splitStack

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -1,12 +1,15 @@
 package com.jaggerabney.csci143;
 
-import java.util.Arrays;
-
-import com.jaggerabney.csci143.week5.practiceit.*;
 import java.util.*;
+import com.jaggerabney.csci143.week6.practiceit.*;
 
 public class Client {
   public static void main(String[] args) {
-    PracticeIt.printSquares(200);
+    Stack<Integer> stack = new Stack<>();
+    stack.addAll(Arrays.asList(-4, -8, 7, 3, -5, 2));
+
+    System.out.println("Before: " + stack);
+    PracticeIt.splitStack(stack);
+    System.out.println("After: " + stack);
   }
 }
--- src/com/jaggerabney/csci143/week6/practiceit/PracticeIt.java
+++ src/com/jaggerabney/csci143/week6/practiceit/PracticeIt.java
@@ -1,5 +1,29 @@
 package com.jaggerabney.csci143.week6.practiceit;
 
-public class PracticeIt {
+import java.util.*;
 
+public class PracticeIt {
+  public static void splitStack(Stack<Integer> stack) {
+    Queue<Integer> queue = new LinkedList<Integer>();
+
+    while (!stack.isEmpty()) {
+      queue.add(stack.pop());
+    }
+
+    for (int i = 0; i < queue.size(); i++) {
+      if (queue.peek() < 0) { // if head is negative
+        stack.push(queue.remove()); // remove from queue, add to stack
+        i--;
+      } else { // if head is positive
+        queue.add(queue.remove()); // move to back of queue
+      }
+    }
+    stack.addAll(queue);
+  }
+
+  public static <T> void reverseQueue(Queue<T> queue) {
+    for (int i = 0; i < queue.size(); i++) {
+      queue.add(queue.remove());
+    }
+  }
 }
