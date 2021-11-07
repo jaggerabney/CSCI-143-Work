e555747a1b60b08edea1d54314d7625f5fd87936
Jagger Abney <jagger.abney@gmail.com>
2021-11-06 22:14:38 -0700

    finished implementation of Exercise20_10 class

--- src/com/jaggerabney/csci143/week6/projects/Exercise20_10.java
+++ src/com/jaggerabney/csci143/week6/projects/Exercise20_10.java
@@ -10,6 +10,7 @@ public class Exercise20_10 {
 
     System.out.println("The union of the two priority queues is " + union(pq1, pq2));
     System.out.println("The difference of the two priority queues is " + difference(pq1, pq2));
+    System.out.println("The intersection of the two priority queues is " + intersection(pq1, pq2));
   }
 
   public static PriorityQueue<String> union(PriorityQueue<String> pq1, PriorityQueue<String> pq2) {
@@ -27,5 +28,16 @@ public class Exercise20_10 {
     result.removeAll(pq2);
 
     return result;
+  }
+
+  public static PriorityQueue<String> intersection(PriorityQueue<String> pq1, PriorityQueue<String> pq2) {
+    PriorityQueue<String> temp = new PriorityQueue<>();
+
+    temp.addAll(pq1);
+    temp.addAll(pq2);
+    temp.retainAll(pq1);
+    temp.retainAll(pq2);
+
+    return new PriorityQueue<String>(new ArrayList<String>(new HashSet<>(temp)));
   }
 }
