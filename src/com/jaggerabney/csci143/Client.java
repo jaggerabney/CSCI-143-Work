beb00384dbc1ce827979d4032b8a4af15b93e098
Jagger Abney <jagger.abney@gmail.com>
2021-11-07 12:52:01 -0800

    first try at EightQueen solution - not working yet

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -6,11 +6,8 @@ import com.jaggerabney.csci143.week6.groupproject.*;
 
 public class Client {
   public static void main(String[] args) {
-    Direction dir = Direction.NORTH;
-
-    do {
-      System.out.println(dir);
-      dir = dir.turnClockwise();
-    } while (!dir.equals(Direction.NORTH));
+    EightQueen eq = new EightQueen();
+    eq.placeQueens();
+    System.out.println(eq);
   }
 }
--- src/com/jaggerabney/csci143/week6/groupproject/EightQueen.java
+++ src/com/jaggerabney/csci143/week6/groupproject/EightQueen.java
@@ -17,11 +17,71 @@ public class EightQueen {
   }
 
   private void placeQueens(int row, int col, int numQueens) {
-    placeQueenAt(row, col);
+    if (numQueens > 0) {
+      placeQueenAt(row, col);
+
+      if (isVulnerable(row, col, 0)) {
+        removeQueenFrom(row, col);
+
+        if (spaceIsBeyondBoundsOfBoard(row + 1, col)) {
+          placeQueens(0, col + 1, numQueens);
+        } else {
+          placeQueens(row + 1, col, numQueens);
+        }
+      } else {
+        numQueens--;
+        System.out.println("Placed queen at (" + row + ", " + col + ")");
+
+        if (spaceIsBeyondBoundsOfBoard(row + 1, col)) {
+          placeQueens(0, col + 1, numQueens);
+        } else {
+          placeQueens(row + 1, col, numQueens);
+        }
+      }
+    }
+  }
+
+  private boolean isVulnerable(int row, int col, int directionIndex) {
+    Direction[] directions = Direction.values();
+
+    if (directionIndex >= directions.length) {
+      return false;
+    } else if (noQueenInDirection(row, col, directions[directionIndex])) {
+      return isVulnerable(row, col, directionIndex + 1);
+    } else {
+      return true;
+    }
+  }
+
+  private boolean noQueenInDirection(int row, int col, Direction direction) {
+    if (spaceIsBeyondBoundsOfBoard(row, col)) {
+      return true;
+    } else if (noQueenAt(row, col)) {
+      return noQueenInDirection(row + direction.rowChange(), col + direction.colChange(), direction);
+    } else {
+      return false;
+    }
+  }
+
+  private boolean spaceIsBeyondBoundsOfBoard(int row, int col) {
+    return (row > 7 || col > 7 || row < 0 || col < 0);
   }
 
   private void placeQueenAt(int row, int col) {
+    System.out.println("(" + row + ", " + col + ")");
     chessboard[row][col] = true;
+  }
+
+  private void removeQueenFrom(int row, int col) {
+    chessboard[row][col] = false;
+  }
+
+  // private boolean queenAt(int row, int col) {
+  // return chessboard[row][col];
+  // }
+
+  private boolean noQueenAt(int row, int col) {
+    return !chessboard[row][col];
   }
 
   @Override
