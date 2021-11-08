98eded673b6263841ea3989333852fa4b1a400e9
Jagger Abney <jagger.abney@gmail.com>
2021-11-07 22:37:54 -0800

    implemented Exercise20_23

--- src/com/jaggerabney/csci143/Client.java
+++ src/com/jaggerabney/csci143/Client.java
@@ -1,11 +1,4 @@
 package com.jaggerabney.csci143;
 
-import com.jaggerabney.csci143.week6.groupproject.*;
-
 public class Client {
-  public static void main(String[] args) {
-    EightQueen eq = new EightQueen();
-    eq.placeQueens();
-    System.out.println(eq);
-  }
 }
