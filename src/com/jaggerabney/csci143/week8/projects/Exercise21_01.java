d00d49a4983f545820a72db3afca7d3002db258a
Jagger Abney <jagger.abney@gmail.com>
2021-11-21 17:03:16 -0800

    implemented difference in Exercise21_01

--- src/com/jaggerabney/csci143/week8/projects/Exercise21_01.java
+++ src/com/jaggerabney/csci143/week8/projects/Exercise21_01.java
@@ -9,8 +9,7 @@ public class Exercise21_01 {
     LinkedHashSet<String> lhs2 = new LinkedHashSet<>(Arrays.asList("George", "Katie", "Kevin", "Michelle", "Ryan"));
 
     System.out.println("The union of the two sets is " + union(lhs1, lhs2));
-    // System.out.println("The difference of the two sets is " + difference(lhs1,
-    // lhs2));
+    System.out.println("The difference of the two sets is " + difference(lhs1, lhs2));
     // System.out.println("The intersection of these two sets is " +
     // intersection(lhs1, lhs2));
   }
@@ -34,7 +33,21 @@ public class Exercise21_01 {
   }
 
   public static LinkedHashSet<String> difference(LinkedHashSet<String> set1, LinkedHashSet<String> set2) {
-    return null;
+    LinkedHashSet<String> workingSet1 = new LinkedHashSet<>(set1);
+    LinkedHashSet<String> workingSet2 = new LinkedHashSet<>(set2);
+    LinkedHashSet<String> elements = new LinkedHashSet<>();
+
+    for (String element : workingSet1) {
+      elements.add(element);
+    }
+
+    for (String element : workingSet2) {
+      if (elements.contains(element)) {
+        elements.remove(element);
+      }
+    }
+
+    return elements;
   }
 
   public static LinkedHashSet<String> intersection(LinkedHashSet<String> set1, LinkedHashSet<String> set2) {
