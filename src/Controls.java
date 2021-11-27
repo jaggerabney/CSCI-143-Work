f2ab705c2191dde0d1eecf77fbff32c6fea62718
Jagger Abney <jagger.abney@gmail.com>
2021-11-27 14:40:06 -0800

    removed println statement from Controls

--- src/Controls.java
+++ src/Controls.java
@@ -11,7 +11,6 @@ public class Controls extends MouseInputAdapter {
 
   @Override
   public void mouseMoved(MouseEvent e) {
-    System.out.println(e);
     this.mousePos = new GPoint(e.getX(), e.getY());
   }
 
