1226c28a2bd68d7b56931b325ccb53bdb3a3b206
Jagger Abney <jagger.abney@gmail.com>
2021-11-27 12:42:56 -0800

    added config.properties

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,10 +1,27 @@
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import java.awt.*;
+import java.util.*;
+import java.io.*;
 
 public class Breakout extends GraphicsProgram {
     @Override
     public void run() {
+        Properties props = loadProps("../resources/config.properties");
+        props.list(System.out);
+    }
 
+    private Properties loadProps(String path) {
+        try {
+            InputStream is = Breakout.class.getResourceAsStream("/resources/config.properties");
+            Properties props = new Properties();
+
+            props.load(is);
+
+            return props;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return null;
+        }
     }
 }
