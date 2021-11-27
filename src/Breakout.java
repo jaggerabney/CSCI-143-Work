9039be2d93e233ccfde0094c2ee6a04716dfc0d9
Jagger Abney <jagger.abney@gmail.com>
2021-11-26 19:02:09 -0800

    added program window

---
+++ src/Breakout.java
@@ -0,0 +1,10 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import java.awt.*;
+
+public class Breakout extends GraphicsProgram {
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/Client.java
@@ -0,0 +1,6 @@
+public class Client {
+  public static void main(String[] args) {
+    Breakout bo = new Breakout();
+    bo.start();
+  }
+}
