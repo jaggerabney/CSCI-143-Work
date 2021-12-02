f379d45f6ae83c43052e12aac19e812314d29b3d
Jagger Abney <jagger.abney@gmail.com>
2021-12-01 18:18:58 -0800

    added constructor and setWidth to Brick

--- src/Brick.java
+++ src/Brick.java
@@ -1,11 +1,20 @@
 import acm.graphics.*;
 
 public class Brick extends GRect {
+  public Brick() {
+    super(0, 0);
+  }
+
   public Brick(double width, double height) {
     super(width, height);
   }
 
   public Brick(double x, double y, double width, double height) {
     super(x, y, width, height);
+  }
+
+  public void setWidth(int windowWidth, int numBricksPerRow, int brickSep) {
+    int width = (windowWidth - (numBricksPerRow - 1) * brickSep) / numBricksPerRow;
+    this.setSize(width, this.getHeight());
   }
 }
